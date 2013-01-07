ee8757e57bb6cab4b201dce1c4d386a99dc6d451
Zack Jones <zjones@ctacorp.com>
2013-01-07 02:40:57 +0000

    trapped exception case

--- src/main/scala/gov/gsa/faq/api/ArticlesResource.scala
+++ src/main/scala/gov/gsa/faq/api/ArticlesResource.scala
@@ -9,7 +9,7 @@ import javax.ws.rs.core.{UriInfo, Context, Response, MediaType}
 import com.wordnik.swagger.annotations.{Api, ApiParam, ApiOperation}
 import gov.gsa.rest.api.dao.InMemoryHSQLDatabase
 import collection.mutable.ListBuffer
-import model.{Results,Result, Articles, Article}
+import model.{Results, Result, Articles, Article}
 import gov.gsa.rest.api.exception.ApiException
 import scala.Array
 import com.wordnik.swagger.jaxrs.Help
@@ -18,31 +18,31 @@ import scala.collection.JavaConversions._
 
 trait ArticlesResource extends RestResourceUtil with RestAPI with LogHelper {
 
-  @Context var uriInfo :UriInfo = _
-  @Context var request :HttpServletRequest = _
-  var rangeFinder : RangeFinder = new RangeFinder()
-  var cmsServices : ArticlesCmsServices = new ArticlesCmsServices()
-  var cmsIdMapper : CmsIdMapper = new CmsIdMapper()
+  @Context var uriInfo: UriInfo = _
+  @Context var request: HttpServletRequest = _
+  var rangeFinder: RangeFinder = new RangeFinder()
+  var cmsServices: ArticlesCmsServices = new ArticlesCmsServices()
+  var cmsIdMapper: CmsIdMapper = new CmsIdMapper()
 
   @GET
   @ApiOperation(value = "Get all Aritcles", notes = "")
   @Path("/articles")
-  override def getResource(@ApiParam(value = "Filter by query param. Ex. \"title::Social Security Administration (SSA)|rank:gt:10.1\"", required = false) @QueryParam("query_filter") queryFilter:String,
-                           @ApiParam(value = "Limit results by property name. Ex. \"id|title|rank\"", required = false) @QueryParam("result_filter") resultFilter:String,
-                           @ApiParam(value = "Sort by property name. Use '-' for descending. Ex. \"id|-rank\"", required = false) @QueryParam("sort") sortParam:String,
-                           @ApiParam(value = "Limit results by range. Ex. \"items=1-101\"", required = false) @HeaderParam("X-Range") rangeHeader:String) : Response = {
+  override def getResource(@ApiParam(value = "Filter by query param. Ex. \"title::Social Security Administration (SSA)|rank:gt:10.1\"", required = false) @QueryParam("query_filter") queryFilter: String,
+                           @ApiParam(value = "Limit results by property name. Ex. \"id|title|rank\"", required = false) @QueryParam("result_filter") resultFilter: String,
+                           @ApiParam(value = "Sort by property name. Use '-' for descending. Ex. \"id|-rank\"", required = false) @QueryParam("sort") sortParam: String,
+                           @ApiParam(value = "Limit results by range. Ex. \"items=1-101\"", required = false) @HeaderParam("X-Range") rangeHeader: String): Response = {
 
-    try{
+    try {
       val faqDao = new FaqDao(InMemoryHSQLDatabase.getInstance(new FaqDatabase()).getDataSource())
       val articles = faqDao.getArticles(queryFilter, resultFilter, sortParam)
       var range = rangeFinder.getRange(articles.size, rangeHeader)
       val articleList = new ListBuffer[Article]()
 
-      if (articles.size>0) {
+      if (articles.size > 0) {
         if (range == null) {
           range = new gov.gsa.rest.api.Range(1, articles.size, articles.size)
         }
-        for(i <- range.start-1 until range.end) {
+        for (i <- range.start - 1 until range.end) {
           articleList += articles(i)
         }
       }
@@ -56,40 +56,45 @@ trait ArticlesResource extends RestResourceUtil with RestAPI with LogHelper {
   @GET
   @ApiOperation(value = "Insert new articles or update existing article by id", notes = "")
   @Path("/articles/cms/update")
-  def updateSelectedCmsArticles(@ApiParam(value = "Article ids. Ex. \"1234|4567\"", required = true) @QueryParam("article_ids") articleIds:String) :Response = {
+  def updateSelectedCmsArticles(@ApiParam(value = "Article ids. Ex. \"1234|4567\"", required = true) @QueryParam("article_ids") articleIds: String): Response = {
 
     val faqDao = new FaqDao(InMemoryHSQLDatabase.getInstance(new FaqDatabase()).getDataSource())
-    updateCmsArticles(faqDao,articleIds)
+    updateCmsArticles(faqDao, articleIds)
   }
 
-  def updateCmsArticles(faqDao: FaqDao, articleIds:String) : Response = {
+  def updateCmsArticles(faqDao: FaqDao, articleIds: String): Response = {
 
-    val results = new ListBuffer[Result]()
-    val ids = if(articleIds!=null && articleIds.length>0) articleIds.split("[|]") else null
+    try {
 
-    if (ids!=null && ids.length>0) {
-      for (articleId <- ids) {
-        val result = new Result()
-        result.id = articleId
-        val article = faqDao.getArticle(articleId)
-        val cmsId = cmsIdMapper.get(article.id)
-        if (cmsId==null) {
-          result.operation = "insert"
-          val _cmsId = cmsServices.createArticle(article)
-          result.result = if (_cmsId==null) "failure" else "success"
-          cmsIdMapper.add(article.id,_cmsId)
-        } else {
-          result.operation = "update"
-          val success = cmsServices.updateArticle(article, cmsId)
-          result.result = if (success) "success" else "failure"
-          if(!success) {
-            logger.error("update of article with id="+article.id+" failed")
+      val results = new ListBuffer[Result]()
+      val ids = if (articleIds != null && articleIds.length > 0) articleIds.split("[|]") else null
+
+      if (ids != null && ids.length > 0) {
+        for (articleId <- ids) {
+          val result = new Result()
+          result.id = articleId
+          val article = faqDao.getArticle(articleId)
+          val cmsId = cmsIdMapper.get(article.id)
+          if (cmsId == null) {
+            result.operation = "insert"
+            val _cmsId = cmsServices.createArticle(article)
+            result.result = if (_cmsId == null) "failure" else "success"
+            cmsIdMapper.add(article.id, _cmsId)
+          } else {
+            result.operation = "update"
+            val success = cmsServices.updateArticle(article, cmsId)
+            result.result = if (success) "success" else "failure"
+            if (!success) {
+              logger.error("update of article with id=" + article.id + " failed")
+            }
           }
+          results += result
         }
-        results += result
       }
+      Response.ok().entity(new Results(results)).build()
+    } catch {
+      case e: Exception => throw new ApiException(e)
     }
-    Response.ok().entity(new Results(results)).build()
   }
 }
 
